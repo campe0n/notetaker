610c8de25a0d9ac9689fe79ed2bb04029c4d2af4
Melvin Finn <melvinrf@Outlook.com>
2021-05-24 11:32:17 -0500

    got the save funciton to work

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require("fs")
-const notes = require("./db/db.json")
+const db = require("./db/db.json")
 const path = require('path');
+const uniqid = require('uniqid')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,11 +13,18 @@ app.use(express.static("public"))
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-router.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')))
 
-
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"))
+    const newNotes = req.body;
+    notes.id = uniqid();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
 
 app.use(router)
 
